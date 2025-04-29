4d98918b1df651aefeb855be7dde6182c70d217a
Luiz Prina <luiz.prina@Luizs-MacBook-Pro.local>
2025-04-29 12:16:58 +0200

    dag corrected

--- dag/hello.py
+++ dag/hello.py
@@ -3,27 +3,27 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'thesimpledata'
-    'start_date': datetime(year:2025, month:4, day:29),
+    'owner': 'thesimpledata',
+    'start_date': datetime(year=2025, month=4, day=29),
     'catchup': False
 }
 
 dag = DAG(
-    dag_id: 'hello_world',
-    default_args = default_args,
-    schedules=timedelta(days=1)
+    dag_id='hello_world',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1)
 )
 
 t1 = BashOperator(
     task_id='hello_world',
-    bash_command = 'echo "Hellow Luiz"
-    dag = dag
+    bash_command='echo "Hello Luiz"',
+    dag=dag
 )
 
 t2 = BashOperator(
-    task_id='hello_fernandp',
-    bash_command = 'echo "Hellow Fernando"
-    dag = dag
+    task_id='hello_fernando',
+    bash_command='echo "Hello Fernando"',
+    dag=dag
 )
 
 t1 >> t2
